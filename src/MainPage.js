5c1976f8c5034c94a4b87a5d8cf8b9dd106a3363
Ethan Lam <ethan.lam@mail.utoronto.ca>
2020-09-07 14:04:52 -0400

    first commit

---
+++ src/MainPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import background from "./main_background.png";
+
+function MainPage() {
+    return <div className="backgroundImage">
+        "Hey, I'm ethan"
+    </div>
+}
+
+export default MainPage;
