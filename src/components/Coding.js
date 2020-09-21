24aed6c8573c9c442f13ed2e93c86d5e0efaa539
Ethan Lam <ethan.lam@mail.utoronto.ca>
2020-09-21 01:27:38 -0400

    finished layout foreal

---
+++ src/components/Coding.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "../styles/Coding.css"
+
+const Coding = props => (
+    <div className="coding-area">
+        <h1>Coding</h1>
+        <span>Click the icons to see more!</span>
+        <div className="coding1">
+            <img src="logo.png" alt="very original"></img>
+            <p> 
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+            </p>
+        </div>
+        <div className="coding1">
+            <img src="logo.png" alt="very original"></img>
+            <p>
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+            </p>
+        </div>
+        <div className="coding1">
+            <img src="logo.png" alt="very original"></img>
+            <p>
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+            </p>
+        </div>
+        <div className="coding1">
+            <img src="logo.png" alt="very original"></img>
+            <p>
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+            </p>
+        </div>
+    </div>
+)
+export default Coding;
---
+++ src/components/Music.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "../styles/Music.css"
+const Music = props => (
+    <div className = "music-section">
+        <h1>Music</h1>
+        <span>Interviewers readings can stop here (unless you want some new tunes)</span>
+        <div className="music-info">
+            <div className="contact-info">
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                    <img src="instagram.png" alt="follow me" />
+                </a>
+                <br />
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                    <img src="youtube.png" alt="follow me" />
+                </a>
+                <br />
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                    <img src="spotify.png" alt="follow me" />
+                </a>
+            </div>
+            <p> Lorem Ipsum heaoueghaldiuvhadsfosa oasdhfaseoif soiv sohoshfos osefhos ohosfsohfoh sow owhfoshfaoseg sp;zhoihsvoiash gos  soghsofgi eihi ih ih fs vosj sfoj sfoj sfoj foj dagoaihdg dsaogh  spisgfpsfjspfojw s</p>
+            <div className="image-area">
+                <img src="placeholder-image.png" alt="yeet" />
+            </div>
+        </div>
+        
+    </div>
+)
+export default Music;
