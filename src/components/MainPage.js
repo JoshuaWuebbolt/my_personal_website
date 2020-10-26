06f33a54f795778cf1b618c60c735ab9bdd6ec43
Ethan Lam <ethan.lam@mail.utoronto.ca>
2020-10-25 20:34:00 -0400

    commented out temp unused variables

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,10 +2,10 @@ import React from "react";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import About from "./About";
-import Coding from "./Coding";
-import Music from "./Music";
-import Videos from "./Videos";
-import Other from "./Other";
+// import Coding from "./Coding";
+// import Music from "./Music";
+// import Videos from "./Videos";
+// import Other from "./Other";
 import "../styles/MainPage.css"
 import Sidebar from "./Sidebar";
 
