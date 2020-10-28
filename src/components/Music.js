2164ab223b965959ef62731329a5ac38a347d17e
Ethan Lam <ethan.lam@mail.utoronto.ca>
2020-10-27 21:53:59 -0400

    updated links

--- src/components/Music.js
+++ src/components/Music.js
@@ -6,15 +6,15 @@ const Music = props => (
         <span>Interviewers reading can stop here (unless you want some new tunes)</span>
         <div className="music-info">
             <div className="music-contact-info">
-                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                <a href="https://www.instagram.com/_revivalmusic/">
                     <img src="instagram.png" alt="follow us" />
                 </a>
                 <br />
-                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                <a href="https://www.youtube.com/channel/UCPD4iZI5-6IkUZc4p1JQw2Q/videos">
                     <img src="youtube.png" alt="watch us" />
                 </a>
                 <br />
-                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                <a href="https://open.spotify.com/artist/44FK2ZMsWgDucYVasjJ5p9?si=EYXDeRgmSEm7OQHLkq_tTw">
                     <img src="spotify.png" alt="stream us " />
                 </a>
             </div>
