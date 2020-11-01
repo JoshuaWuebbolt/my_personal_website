58de3cdda939530125c2c6cd0a3bd90c01b93614
Ethan Lam <ethan.lam@mail.utoronto.ca>
2020-10-31 22:37:12 -0400

    add BrowserRouter routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from "react-router-dom";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainPage from "./MainPage"
 import Blog from "./Blog"
 import MakingWebsites from "./Blogs/MakingWebsites";
@@ -7,11 +7,13 @@ import MakingWebsites from "./Blogs/MakingWebsites";
 function App() {
   return (
     <div className="app">
-      <Switch>
-        <Route path="/" component={MainPage} exact/>
-        <Route path="/blog" component = {Blog} exact/>
-        <Route path="/blog/making-websites" component = {MakingWebsites} exact/>
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={MainPage} exact/>
+          <Route path="/blog" component = {Blog} exact/>
+          <Route path="/blog/making-websites" component = {MakingWebsites} exact/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import '../styles/NavBar.css';
-
+import {Link} from "react-router-dom"
 
 function NavBar(props) {
     return <nav className="nav-bar">
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="home">Ethan Lam</a>
-        <div className="mobile"></div>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="desktop">About Me </a>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="desktop">Coding</a>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="desktop">Music</a>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="desktop">Videos</a>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="desktop">Other</a>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="desktop">Blog</a>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="home">Ethan Lam</a>
+            <div className="mobile"></div>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="desktop">About Me </a>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="desktop">Coding</a>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="desktop">Music</a>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="desktop">Videos</a>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="desktop">Other</a>
+            <Link to="/blog">Blog</Link>
     </nav>
+
 }
 
 export default NavBar;
