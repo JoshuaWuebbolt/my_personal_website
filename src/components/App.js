3db9ca03d4a334494eef3f8c22052484511c7c11
Ethan Lam <ethan.lam@mail.utoronto.ca>
2020-11-17 23:55:43 -0500

    blog

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainPage from "./MainPage"
 import Blog from "./Blog"
 import MakingWebsites from "./Blogs/MakingWebsites";
+import CriticalReview from "./Blogs/CriticalReview";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" component={MainPage} exact/>
           <Route path="/blog" component = {Blog} exact/>
           <Route path="/blog/making-websites" component = {MakingWebsites} exact/>
+          <Route path="/blog/critical-review" component = {CriticalReview} exact/>
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,18 +5,19 @@ import "../styles/Blog.css"
 const Blog = (props) => (
   <div className="blog">
     <nav className="blog-nav"> 
-      <h2>Stuff I Write</h2>
+      <Link to="/blog"> 
+        <h2>Stuff I Write</h2>
+      </Link>
       <Link to="/"> 
         <img src="assets/home.png" alt="go home"/>
       </Link>
     </nav>
     <ul className="blog-list">
-      <Link to="/blog/making-websites"> 
+      <Link to="/blog/critical-review"> 
         <div className ="blogpost-area">
-          <span className="date">Oct 29. 2020</span>
-          <h1 className="title">Building Websites for Beginners: By a Beginner</h1>
-          <span className="length"><i>Length: ~2min</i></span>
-          <p> hello</p>
+          <span className="date">Nov 17. 2020</span>
+          <h1 className="title">Critically Reviewing an Article That’s Just a Critical Review of Interviews, Which Are Really Just Critical Reviews of People</h1>
+          <span className="length"><i>Length: ~3min</i></span>
         </div>
       </Link>
       <Link to="/blog/making-websites"> 
@@ -24,7 +25,6 @@ const Blog = (props) => (
           <span className="date">Oct 29. 2020</span>
           <h1 className="title">Building Websites for Beginners: By a Beginner</h1>
           <span className="length"><i>Length: ~2min</i></span>
-          <p> hello</p>
         </div>
       </Link>
     </ul>
