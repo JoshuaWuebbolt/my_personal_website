0718fbda6bba1b535d38a8380e9a88edd40a2473
Ethan Lam <ethan.lam@mail.utoronto.ca>
2020-12-20 15:53:20 -0500

    fix sidebar spacing and scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,17 +7,21 @@ function NavBar(props) {
     const scrollToTop = () => {
       animateScroll.scrollToTop();
     };
+    
 
-
-    return <nav className="nav-bar">
+    return <nav className="nav-bar" >
+      
             <button className="home" onClick={scrollToTop}><h2>Ethan Lam</h2></button>
             <div className="mobile"></div>
+            
             <Link to={"about-section"} smooth={true} duration={1000} offset={-50} className="desktop">About</Link>
             <Link to={"coding-section"} smooth={true} duration={1000} offset={-50} className="desktop">Coding</Link>
             <Link to={"music-section"} smooth={true} duration={1000} offset={-50} className="desktop">Music</Link>
             <Link to={"video-section"} smooth={true} duration={1000} offset={-50} className="desktop">Videos</Link>
             <Link to={"other-section"} smooth={true} duration={1000} offset={-50} className="desktop">Other</Link>
             <Direct to="/blog" className="desktop">Blog</Direct>
+
+            
     </nav>
 
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,9 +5,12 @@ import {Link as Direct} from "react-router-dom"
 
 function Sidebar(props) {
     const [xPosition, setX] = useState(100);
+    const [scrollLock, setScrollLock] = useState("")
     function triggerSidebar() {
         setX(xPosition === 100 ? 0:100);
+        setScrollLock(xPosition === 100? "hidden":"")
     }
+    document.body.style.overflow = scrollLock
     return <div className="sidebar">
         <button onClick={triggerSidebar}> 
                 <img src="assets/menu.png" alt="menu"/>
@@ -20,6 +23,7 @@ function Sidebar(props) {
             <Link to={"video-section"} smooth={true} duration={1000} offset={-50} onClick={triggerSidebar}>Videos</Link>
             <Link to={"other-section"} smooth={true} duration={1000} offset={-50} onClick={triggerSidebar}>Other</Link>
             <Direct to="/blog">Blog</Direct>
+            <div className="spacer"> </div>
         </div>
     </div>
 }
