2a0a72b1a49fdd4d95c84c8da72179a88b33204c
Joshua Jude Wuebbolt <joshua.wuebbolt@mail.utoronto.ca>
2024-07-12 13:03:58 -0400

    Made small change to typist in hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,25 +10,25 @@ const Hero = props => (
         <div className="hero-box">
             <div className="text-line">
                 <div className="spacer"/>
-                    <h1>Hi. My name's Ethan,</h1>
+                    <h1>Hi. My name's Joshua,</h1>
             </div>
             <div className="text-line">
                 <div className="spacer" />
-                    <h1>and I make</h1>
+                    <h1>and I am a</h1>
             </div>
             <div className="text-line">
                 <div className="spacer" />
                 <h1>
                     <Typist startDelay={2000} cursor={{hideWhenDone:true, hideWhenDoneDelay:3000}}>
-                    code
-                    <Typist.Backspace count={4} delay={1000}/>
-                    music
-                    <Typist.Backspace count={5} delay={1000} />
-                    videos
-                    <Typist.Backspace count={6} delay={1000} />
-                    bad decisions :)
-                    <Typist.Backspace count={16} delay={500} />
-                    stuff.
+                    programmer
+                    <Typist.Backspace count={10} delay={1000}/>
+                    urbanist
+                    <Typist.Backspace count={8} delay={1000} />
+                    student
+                    <Typist.Backspace count={11} delay={1000} />
+                    cool guy :)
+                    <Typist.Backspace count={11} delay={1000} />
+                    full stack developer
                     </Typist>
                 </h1>
             </div>
