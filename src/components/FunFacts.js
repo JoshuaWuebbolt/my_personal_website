3a21832a9ca7627665bcabdd16de11c2573033f5
Joshua Jude Wuebbolt <joshua.wuebbolt@mail.utoronto.ca>
2025-01-15 21:47:13 -0500

    Fixed typos in the FunFacts section

--- src/components/FunFacts.js
+++ src/components/FunFacts.js
@@ -9,9 +9,9 @@ const FunFacts = props => (
         <div id="scroll-container">
             <div id="scroll-text">
                 I held a world record for 2 weeks<br></br><br></br>
-                I can make an Ethernet cable from scratch, the longest one I made was 60 metres (about 200ft)<br></br><br></br>
-                I've compleated The Legend of Zelda: Ocarina of Time<br></br><br></br>
-                I absolutely hate kinddom hearts<br></br><br></br>
+                I can make an Ethernet cable from scratch; the longest one I made was 60 metres (about 200ft)<br></br><br></br>
+                I have completed  The Legend of Zelda: Ocarina of Time<br></br><br></br>
+                I absolutely hate kingdom hearts<br></br><br></br>
                 I can hold my breath for 2 minuets 46 seconds<br></br><br></br>
                 I have never flown on a plane<br></br><br></br>
                 I held a world record for 2 weeks<br></br><br></br>
